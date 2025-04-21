f82e8661a09281963b40625fd8fd9064d1318096
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-04-21 19:52:51 +0600

    Length of a string using strlen

--- length_of_string.c
+++ length_of_string.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char s[101];
     scanf("%s", s);
 
-    int count = 0;
+    // int count = 0;
 
-    while (s[count] != '\0')
-    {
-        count++;
-    };
+    // while (s[count] != '\0')
+    // {
+    //     count++;
+    // };
 
-    printf("%d", count);
+   int size = strlen(s);
+
+    printf("%d", size);
 
     return 0;
 };
