dc57086e903d29a941faa7a1178667438415b06c
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-04-22 14:35:55 +0600

    Square or rectangle

---
+++ A_Square_or_rectangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        int w, h;
+
+        scanf("%d %d", &w, &h);
+
+        if (w == h)
+        {
+            printf("Square\n");
+        }
+        else
+        {
+            printf("Rectangle\n");
+        }
+    }
+
+    return 0;
+};
